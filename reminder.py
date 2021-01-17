36bcd325e3b3a19e9f51783a2cd8f59fdb480484
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2021-01-17 12:23:15 +0530

    Update reminder.py

--- reminder.py
+++ reminder.py
@@ -7,14 +7,15 @@ from plyer import notification
 #toaster = ToastNotifier()
 #toaster.show_toast()
 
-#to get date from user
-#from dateutil import parser
-#date = parser.parse(input("Enter date: "))
-
 label=input('Enter the label for the remainder: ')
+
 
 date_time = str(input('Enter date and time: (yyyy-mm-dd hh:mm): '))
 my_date = datetime.strptime(date_time, "%Y-%m-%d %H:%M")
+
+#alternate way to get date from user
+#from dateutil import parser
+#date = parser.parse(input("Enter date: "))
 
 # datetime object containing current date and time
 now_date = datetime.now()
